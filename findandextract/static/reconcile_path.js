4bb20d3912e7dd3df1da1b9105ddda7cc2f0c0a5
connorneu <connorneuendorff@gmail.com>
2023-12-16 23:51:03 -0500

    reconcile added multiple matching

--- findandextract/static/reconcile_path.js
+++ findandextract/static/reconcile_path.js
@@ -1,8 +1,8 @@
 async function start_reconcile(){
-    center_match_columns();
+    
     await add_to_carousel(['Select files to reconcile:'], action_color, ["document.getElementById('carouselcontainer" + (carousel_num) +"').classList.add('action')"], false, false);
     document.getElementById('reconcilefiles').style.display = 'block';
-    
+    center_match_columns();
 }
 
 function center_match_columns(){
@@ -13,14 +13,17 @@ function center_match_columns(){
     var leftPos = left_parent.getBoundingClientRect();
     var rightPos = right_parent.getBoundingClientRect();
     for (var i=0;i<left_children.length;i++){
+        console.log('here')
         var left_child_pos = left_children[i].getBoundingClientRect();
         var right_child_pos = right_children[i].getBoundingClientRect();
         left_children[i].style.left = (leftPos.left - left_child_pos.left) + ((leftPos.width - left_child_pos.width) / 2)   + "px";
         right_children[i].style.left = (rightPos.left - right_child_pos.left) + ((rightPos.width - right_child_pos.width) / 2)   + "px";
     }
-    
-    //centerDiv.style.top = targetPos.bottom + "px";
-    //centerDiv.style.left = targetPos.left + (target.offsetWidth - centerDiv.offsetWidth) / 2 + "px";
-    //console.log(targetPos.left + (target.offsetWidth - centerDiv.offsetWidth) / 2)
 }
 
+$(document.body).on('click', '#reco-add-match' ,async function(){ 
+    var colmatch_parent = document.getElementById("reco-all-cols-to-match");
+    var clone_match = colmatch_parent.getElementsByClassName('reco-col-match')[0].cloneNode(true);
+    colmatch_parent.appendChild(clone_match);
+
+}); 
