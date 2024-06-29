885fc90dedcce80ed93f334d87fc9e3ce3b15ad3
connorneu <connorneuendorff@gmail.com>
2024-06-29 10:25:59 -0400

    wrongdyno

--- findandextract/views.py
+++ findandextract/views.py
@@ -161,9 +161,6 @@ def findandextract(request):
                     find_col_3 = params['findcol3'].rstrip().strip()
                     search_where = [[find_file_1, find_col_1], [find_file_2, find_col_2], [find_file_3, find_col_3]]
                     pub_df_result = Extract(input_or_description, extract_file_name, extract_col_name, describe_values, search_where, request.user)
-                    #columns = ['Student ID', 'Course ID', 'Marks']
-                    #data = [(103, 201, 67), (103, 203, 67), (103, 204, 89)]
-                    #df_result = pd.DataFrame(data, columns=columns)
                     print('------------- RESULT --------------')
                     print(pub_df_result)
                     write_results(pub_df_result, pub_df_result.head(display_table_row_num), str(request.user))
